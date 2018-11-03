2083565f5d4a380bbff4a66783717346266c13bb
Katelyn Peterson <00154664@badgermail.snow.edu>
2018-11-02 19:28:21 -0600

    add test class

---
+++ KPAvaloniaApp/AvaloniaTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AvaloniaTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AvaloniaTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f1851193-4ecd-44dd-8fd5-6c911ba995dd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ KPAvaloniaApp/AvaloniaTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NUnit.Framework;
+using Demo;
+using Interfaces;
+using FluentAssertions;
+
+namespace AvaloniaTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void OpenAnImageFile()
+        {
+            var dataServiceMock = new Mock<IDataService>();
+        }
+
+        [TestMethod]
+        public void CallWebAPI()
+        {
+
+        }
+    }
+}
