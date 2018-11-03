d451b51ed7db0eb6915ba43da73eee3b58ee0512
Katelyn Peterson <00154664@badgermail.snow.edu>
2018-11-03 11:40:19 -0600

    add new test project, clean up nuget packages and dependencies, start adding to web api tab

---
+++ KPAvaloniaApp/AvaloniaTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using System;
+using KPAvalonia;
+using Interfaces;
+using Moq;
+using FluentAssertions;
+
+namespace AvaloniaTests
+{
+    public class UnitTest1
+    {
+        [Test]
+        public void OpenImageFile()
+        {
+
+        }
+
+        [Test]
+        public void CallWebAPI()
+        {
+
+        }
+    }
+}
